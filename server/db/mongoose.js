8104b52aae821d70da785543818c4a08d44aeecd
Antonio Erdeljac <erdeljac.antonio@gmail.com>
2019-01-31 19:08:12 +0100

    Fix deprecation warning

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const config = require('../config');
 
 mongoose.Promise = Promise;
+mongoose.set('useCreateIndex', true);
 mongoose.connect(config.mongodb.uri, { useNewUrlParser: true });
 
 module.exports.mongoose = mongoose;
